6b0e59a5d2665a6f842a5cf97e6546abbd0dc58a
Usmanovsky <ussy90@gmail.com>
2021-02-09 23:21:11 -0500

    Added ability to choose number of molecules You can now select how many mols of compounds A and B to insert in the topolgy file. sample: python3 des_renamer.py folder1 folder2 molsA molsB

--- DES-IRC/des_renamer.py
+++ DES-IRC/des_renamer.py
@@ -1,4 +1,9 @@
-# This creates system files and copies all the top file to each DES folder
+# This creates DES simulation files from components kept in two different folders.
+# Run the script using "python3 des_renamer.py DES/Compound 1/* DES/Compound 2/* 100 100"
+# The above will create binary systems from each folder path and insert 100 mols of each
+# Your directories might be different so set them up accordingly.
+# created by ulab222
+
 from pathlib import Path
 from itpandgro import filesrenamer
 import os, shutil, sys
@@ -19,18 +24,28 @@ if __name__ == "__main__":  # Accept command line inputs for  ligpargen files on
         res1_path = "DES/Compound 1/*"  # If empty, all the folders in ligpargen folder will be worked on
     
     
-    if len(sys.argv[1]) > 1:
+    if len(sys.argv[2]) > 1:
         res2_path = str(sys.argv[2])
     else:
         res2_path = "DES/Compound 2/*"
+        
+    if len(sys.argv[3]) > 1:
+        molnumx = str(sys.argv[3])  # Number of molecules for Compound A
+    else:
+        molnumx = 50
+        
+                
+    if len(sys.argv[4]) > 1:
+        molnumy = str(sys.argv[4])  # Number of molecules for Compound B
+    else:
+        molnumy = 50
    
 
 # These are placeholders that are replaced in each DES top file
 x = 'molx'
 y = 'moly'
-
-# res1_path = "DES/Compound 1/*"
-# res2_path = "DES/Compound 2/*"
+numx = 'molnumx'
+numy = 'molnumy'
 
 # Set the folder name where the DES folders will be created in.
 X = "X"
@@ -50,55 +65,68 @@ for folder1 in pathway.glob(res1_path):
 #             print(folder2)
             res2 = rewrite(folder2)  # The name of compound B
 #             print(res2)
-            des_name = res1 + "-" + res2 + "11"  # Set DES folder name
-            print(des_name)
-#             os.mkdir(f"X")        
-            os.mkdir(f"{X}/{des_name}")  
-            for number in range(1,4):  # Copying mdp and topology files              
-                mdp_path = des_name + str(number)
-                final_path = os.path.join(f"{X}/{des_name}", f"{mdp_path}.mdp")
-                print(final_path)
-                print("\n")
-                shutil.copy(f"{smallsys}/{number}.mdp", final_path)
+            if res1 == res2:
+                pass
+            else:                
+                des_name = res1 + "-" + res2 + "11"  # Set DES folder name
+                print(des_name)
+    #             os.mkdir(f"X")        
+                try:
+                    os.mkdir(f"{X}/{des_name}")
+                except FileExistsError:
+                    print(f"{X}/{des_name} already exists") 
+                    
+                for number in range(1,4):  # Copying mdp and topology files              
+                    mdp_path = des_name + str(number)
+                    final_path = os.path.join(f"{X}/{des_name}", f"{mdp_path}.mdp")
+                    print(final_path)
+                    print("\n")
+                    shutil.copy(f"{smallsys}/{number}.mdp", final_path)
+                    
+                    top_name = des_name  # set topology file name 
+                    top_path = os.path.join(f"{X}/{des_name}", f"{top_name}.top")
+                    print(top_path)
+                    print("\n")
+                    shutil.copy(f"{smallsys}/topology.top", top_path)
+                    os.system(f"sed -i 's/{x}/{res1}/g' {top_path}")
+                    os.system(f"sed -i 's/{y}/{res2}/g' {top_path}")
+                    os.system(f"sed -i 's/{numx}/{molnumx}/g' {top_path}")
+                    os.system(f"sed -i 's/{numy}/{molnumy}/g' {top_path}")
+                    
+                # Copying itp and gro files using the imported filesrenamer method. This works with the assumption
+                # that all itp and gro files are kept in a folder called ligpargen. Set ligpargen variable to your folder name, if different.
+                itp1_path = os.path.join(f"{X}/{des_name}", f"{res1}.itp")
+                itp2_path = os.path.join(f"{X}/{des_name}", f"{res2}.itp")
+                gro1_path = os.path.join(f"{X}/{des_name}", f"{res1}.gro")
+                gro2_path = os.path.join(f"{X}/{des_name}", f"{res2}.gro")
                 
-                top_name = des_name  # set topology file name 
-                top_path = os.path.join(f"{X}/{des_name}", f"{top_name}.top")
-                print(top_path)
-                print("\n")
-                shutil.copy(f"{smallsys}/topology.top", top_path)
-                os.system(f"sed -i 's/{x}/{res1}/g' {top_path}")
-                os.system(f"sed -i 's/{y}/{res2}/g' {top_path}")
+                if os.path.isfile(f"{ligpargen}/{res1}/{res1}.itp") and os.path.isfile(f"{ligpargen}/{res1}/{res1}.gro"):  # 
+                    #itp_path = os.path.join(f"X/{des_name}", f"{res1}.itp")
+                    shutil.copy(f"{ligpargen}/{res1}/{res1}.itp", itp1_path)
+                    shutil.copy(f"{ligpargen}/{res1}/{res1}.gro", gro1_path)
+                else:
+                    folderpath = f"{ligpargen}/{res1}"
+                    filesrenamer(folderpath)
+                    shutil.copy(f"{ligpargen}/{res1}/{res1}.itp", itp1_path)
+                    shutil.copy(f"{ligpargen}/{res1}/{res1}.gro", gro1_path)
                 
-            # Copying itp and gro files using the imported filesrenamer method. This works with the assumption
-            # that all itp and gro files are kept in a folder called ligpargen. Set ligpargen variable to your folder name, if different.
-            itp1_path = os.path.join(f"{X}/{des_name}", f"{res1}.itp")
-            itp2_path = os.path.join(f"{X}/{des_name}", f"{res2}.itp")
-            gro1_path = os.path.join(f"{X}/{des_name}", f"{res1}.gro")
-            gro2_path = os.path.join(f"{X}/{des_name}", f"{res2}.gro")
-            
-            if os.path.isfile(f"{ligpargen}/{res1}/{res1}.itp") and os.path.isfile(f"{ligpargen}/{res1}/{res1}.gro"):  # 
-                #itp_path = os.path.join(f"X/{des_name}", f"{res1}.itp")
-                shutil.copy(f"{ligpargen}/{res1}/{res1}.itp", itp1_path)
-                shutil.copy(f"{ligpargen}/{res1}/{res1}.gro", gro1_path)
-            else:
-                folderpath = f"{ligpargen}/{res1}"
-                filesrenamer(folderpath)
-                shutil.copy(f"{ligpargen}/{res1}/{res1}.itp", itp1_path)
-                shutil.copy(f"{ligpargen}/{res1}/{res1}.gro", gro1_path)
-            
-            
-            if os.path.isfile(f"{ligpargen}/{res2}/{res2}.itp") and os.path.isfile(f"{ligpargen}/{res2}/{res2}.gro"):
-                #itp_path = os.path.join(f"X/{des_name}", f"{res2}.itp")
-                shutil.copy(f"{ligpargen}/{res2}/{res2}.itp", itp2_path)
-                shutil.copy(f"{ligpargen}/{res2}/{res2}.gro", gro2_path)
-            else:
-                folderpath = f"{ligpargen}/{res2}"
-                filesrenamer(folderpath)
-                shutil.copy(f"{ligpargen}/{res2}/{res2}.itp", itp2_path)
-                shutil.copy(f"{ligpargen}/{res2}/{res2}.gro", gro2_path)
                 
-            
-            shutil.copytree(f"{oplsaa}/", f"{X}/{des_name}/oplsaa.ff")  # This only works if there is an oplsaa.ff folder in the working directory
+                if os.path.isfile(f"{ligpargen}/{res2}/{res2}.itp") and os.path.isfile(f"{ligpargen}/{res2}/{res2}.gro"):
+                    #itp_path = os.path.join(f"X/{des_name}", f"{res2}.itp")
+                    shutil.copy(f"{ligpargen}/{res2}/{res2}.itp", itp2_path)
+                    shutil.copy(f"{ligpargen}/{res2}/{res2}.gro", gro2_path)
+                else:
+                    folderpath = f"{ligpargen}/{res2}"
+                    filesrenamer(folderpath)
+                    shutil.copy(f"{ligpargen}/{res2}/{res2}.itp", itp2_path)
+                    shutil.copy(f"{ligpargen}/{res2}/{res2}.gro", gro2_path)
+                    
+                
+                
+                try:
+                    shutil.copytree(f"{oplsaa}/", f"{X}/{des_name}/oplsaa.ff")  # This only works if there is an oplsaa.ff folder in the working directory
+                except FileExistsError:
+                    print(f"{X}/{des_name}/oplsaa.ff already exists")
               
                 
                 
