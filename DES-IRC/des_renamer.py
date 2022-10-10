ab99f1be79c5affba1c26ca781ff8ed2aab142a1
ulab20464 <ulab20464@gmail.com>
2022-10-10 08:57:23 -0400

    added the modified des_renamer.py

--- DES-IRC/des_renamer.py
+++ DES-IRC/des_renamer.py
@@ -30,27 +30,27 @@ if __name__ == "__main__":  # Accept command line inputs for  ligpargen files on
         res2_path = "DES/Compound 2/*"
         
     if len(sys.argv[3]) > 1:
-        molnumx = str(sys.argv[3])  # Number of molecules for Compound A
+        molnumx = int(sys.argv[3])  # Number of molecules for Compound A
     else:
         molnumx = 50
         
                 
     if len(sys.argv[4]) > 1:
-        molnumy = str(sys.argv[4])  # Number of molecules for Compound B
+        molnumy = int(sys.argv[4])  # Number of molecules for Compound B
     else:
         molnumy = 50
    
 # calculate molar ratios
 try:
-    a = int(molnumx)
-    b = int(molnumy)
+    a = molnumx
+    b = molnumy
     if a > b:    
-        ratioA = str(a/b)
-        ratioB = str(b/b)
+        ratioA = str(int(a/b))
+        ratioB = str(int(b/b))
         ratio = ratioA + ratioB
     elif b > a:
-        ratioA = str(a/a)
-        ratioB = str(b/a)
+        ratioA = str(int(a/a))
+        ratioB = str(int(b/a))
         ratio = ratioA + ratioB
     else:
         ratio = "11"
@@ -64,6 +64,7 @@ x = 'molx'
 y = 'moly'
 numx = 'molnumx'
 numy = 'molnumy'
+title = 'sim-title'
 
 # Set the folder name where the DES folders will be created in.
 X = "X"
@@ -94,22 +95,25 @@ for folder1 in pathway.glob(res1_path):
                 except FileExistsError:
                     print(f"{X}/{des_name} already exists") 
                     
-                for number in range(1,4):  # Copying mdp and topology files              
-                    mdp_path = des_name + str(number)
+                for number in range(1,4):  # Copying mdp and topology files.               
+                    # mdp_path = des_name + str(number)  # pick this if you want your mdp files to be named like Dea-Men111.mdp
+                    mdp_path = str(number)  # pick this if you want your mdp files to be named like 1.mdp
                     final_path = os.path.join(f"{X}/{des_name}", f"{mdp_path}.mdp")
                     print(final_path)
                     print("\n")
                     shutil.copy(f"{smallsys}/{number}.mdp", final_path)
+
                     
-                    top_name = des_name  # set topology file name 
-                    top_path = os.path.join(f"{X}/{des_name}", f"{top_name}.top")
-                    print(top_path)
-                    print("\n")
-                    shutil.copy(f"{smallsys}/topology.top", top_path)
-                    os.system(f"sed -i 's/{x}/{res1}/g' {top_path}")
-                    os.system(f"sed -i 's/{y}/{res2}/g' {top_path}")
-                    os.system(f"sed -i 's/{numx}/{molnumx}/g' {top_path}")
-                    os.system(f"sed -i 's/{numy}/{molnumy}/g' {top_path}")
+                top_name = des_name  # set topology file name 
+                top_path = os.path.join(f"{X}/{des_name}", f"{top_name}.top")
+                print(top_path)
+                print("\n")
+                shutil.copy(f"{smallsys}/topology.top", top_path)
+                os.system(f"sed -i 's/{x}/{res1}/g' {top_path}")
+                os.system(f"sed -i 's/{y}/{res2}/g' {top_path}")
+                os.system(f"sed -i 's/{title}/{des_name}/g' {top_path}")
+                os.system(f"sed -i 's/{numx}/{molnumx}/g' {top_path}")
+                os.system(f"sed -i 's/{numy}/{molnumy}/g' {top_path}")
                     
                 # Copying itp and gro files using the imported filesrenamer method. This works with the assumption
                 # that all itp and gro files are kept in a folder called ligpargen. Set ligpargen variable to your folder name, if different.
