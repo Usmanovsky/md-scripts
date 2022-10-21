3042c5e4c7bc53fd8ece0a9acd2f97ba70f4303c
Usman Abbas <ussy90@gmail.com>
2022-10-21 16:59:15 -0400

    Discover which pdb files are problematic

--- alpha-fold/dat-and-csv-generation/error-sniffer.sh
+++ alpha-fold/dat-and-csv-generation/error-sniffer.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # This finds *.out files with errors (letters in the PLDDT column)
-# how-to-run: ./error-sniffer.sh >> name-of-log-file-to-store-results
-# This assumes the script is in the same directory as  th eoutput-data-* folders.
+# how-to-run: ./error-sniffer.sh name-of-out-folder >> name-of-log-file-to-store-results
+# If you do not pass a name, the default is output-data-*
+# This assumes the script is in the same directory as the output-data-* folders.
 
-find ./output-data-* -type f -name "AF*.out" | xargs -I {} -P 1000 grep -l ' C ~' {};
+t="./output-data-*"
+target=${1:-$t}
+find $target -type f -name "AF*.out" | xargs -I {} -P 1000 grep -l ' C ~' {};
