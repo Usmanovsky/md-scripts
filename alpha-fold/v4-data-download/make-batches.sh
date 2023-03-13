9b37752905b8f984d57bd9891297163eafb8c4a6
Usman Abbas <ussy90@gmail.com>
2023-03-13 12:47:46 -0400

    download scripts for AF v4 DB

---
+++ alpha-fold/v4-data-download/make-batches.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# This assumes you've already used select-samples.py to create batch1.csv.
+# This script will then generate unique batches 2-5
+
+# make batch2
+python3 find-diff-pandas.py v4_updated_accessions.txt batch1.csv not-in-batch-1.csv
+python3 select-samples.py not-in-batch-1.csv batch2.csv
+
+#make batch3
+python3 find-diff-pandas.py not-in-batch-1.csv batch2.csv not-in-batch-2.csv
+python3 select-samples.py not-in-batch-2.csv batch3.csv
+
+#make batch4
+python3 find-diff-pandas.py not-in-batch-2.csv batch3.csv not-in-batch-3.csv
+python3 select-samples.py not-in-batch-3.csv batch4.csv
+
+#make batch5
+python3 find-diff-pandas.py not-in-batch-3.csv batch4.csv not-in-batch-4.csv
+python3 select-samples.py not-in-batch-4.csv batch5.csv
