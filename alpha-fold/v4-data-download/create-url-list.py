5af317e55f7fe5a3f600b062471c35f550cd458a
Usman Abbas <ussy90@gmail.com>
2023-03-14 17:32:39 -0400

    Updated v4 DB link

--- alpha-fold/v4-data-download/create-url-list.py
+++ alpha-fold/v4-data-download/create-url-list.py
@@ -8,6 +8,7 @@ file_out=open(sys.argv[2],"w")
 
 with open(sys.argv[1]) as f:
     for line in f:
-      uniprot=line[:-1]
-      url="https://alphafold.ebi.ac.uk/files/AF-"+uniprot+"-F1-model_v4.pdb"
+      uniprot=line[:-1]     
+      line=line.strip("\n") + "-model_v4.pdb"  # v4 AF DB
+      url=f"https://alphafold.ebi.ac.uk/files/{line}"      
       print(url,file=file_out)
