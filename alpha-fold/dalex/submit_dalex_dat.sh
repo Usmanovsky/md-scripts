498151149ce2bf8b7e115632ed10d04d3cfee3a9
Usman Abbas <ussy90@gmail.com>
2023-03-20 14:32:02 -0400

    fixed dssp/dat naming convention to avoid future clashes. Same for total merger jobs that might exclude some csv/dat files.

--- alpha-fold/dalex/submit_dalex_dat.sh
+++ alpha-fold/dalex/submit_dalex_dat.sh
@@ -7,9 +7,9 @@
 #SBATCH --partition=normal          # Partition/queue to run the job in. (REQUIRED)
 #SBATCH -e slurm-dalex-res5-%j.err             # Error file for this job.
 #SBATCH -o slurm-dalex-res5-%j.out             # Output file for this job.
-#SBATCH -A coa_qsh226_uksr       # Project allocation account name (REQUIRED)
+#SBATCH -A xxx       # Project allocation account name (REQUIRED)
 #SBATCH --mail-type ALL         # Send email when job starts/ends
-#SBATCH --mail-user ulab222@uky.edu   # Where email is sent to (optional)
+#SBATCH --mail-user xxx@xxx   # Where email is sent to (optional)
 
 module load Miniconda3
 conda init
--- alpha-fold/re-download/dssp-pdb2dat.sh
+++ alpha-fold/re-download/dssp-pdb2dat.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # ulab 09/30/2022
 # This uses dssp to get secondary structure info from pdb files
-# and store it in dat files. Make sure you install dssp on Linux
+# and store it in dssp files. Make sure you install dssp on Linux
 # or in conda
 # $1 is the location of the pdb-folders.
 find $1 -maxdepth 2 -mindepth 1 -type f -name "*.pdb" | while read dir; do
 #echo $dir
 dat=${dir%.*}
 echo $dat
-mkdssp $dir >> $dat.dat
+mkdssp $dir >> $dat.dssp  #dat (changed to dssp to avoid AF*dat and residue dat files)
 done
